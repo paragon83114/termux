cb1aaacc76cf6af64345ed520e416e88266db0ec
paragon83114 <155473681+paragon83114@users.noreply.github.com>
2026-05-17 12:59:16 +0200

    Update installar_proot.sh

--- installar_proot.sh
+++ installar_proot.sh
@@ -6,6 +6,8 @@ pkg upgrade -y -o Dpkg::Options::="--force-confnew" &>/dev/null
 sed -i 's/^# fullscreen = true/fullscreen = true/' ~/.termux/termux.properties
 sed -i 's/^# *extra-keys = \[\[ESC.*/extra-keys = []/' ~/.termux/termux.properties
 sed -i 's/^# back-key=escape/back-key=escape/' ~/.termux/termux.properties
+[ ! -e ~/.termux/font.ttf ] && curl -L https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Ligatures/Regular/JetBrainsMonoNerdFont-Regular.ttf -o ~/.termux/font.ttf
+termux-reload-settings
 pkg install -y proot-distro
 proot-distro install debian
 echo "proot-distro login debian" > .bashrc
