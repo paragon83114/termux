1fd18631c641a674a18e78230c28f3559cfc8d77
paragon83114 <155473681+paragon83114@users.noreply.github.com>
2026-05-17 11:05:09 +0200

    Add files via upload

---
+++ install_proot.sh
@@ -0,0 +1,11 @@
+[ ! -e ~/storage ] && termux-setup-storage
+touch ~/.hushlogin
+termux-change-repo
+pkg update &>/dev/null
+pkg upgrade -y -o Dpkg::Options::="--force-confnew" &>/dev/null
+sed -i 's/^# fullscreen = true/fullscreen = true/' ~/.termux/termux.properties
+sed -i 's/^# *extra-keys = \[\[ESC.*/extra-keys = []/' ~/.termux/termux.properties
+sed -i 's/^# back-key=escape/back-key=escape/' ~/.termux/termux.properties
+pkg install -y proot-distro
+proot-distro install debian
+echo "proot-distro login debian 2>/dev/null" > .bashrc
